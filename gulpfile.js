a3f955a039ef1676e25a3f91499066aa94739bb9
sharvarikapdi <sharvarikapdi@users.noreply.github.com>
2019-07-07 19:39:49 +0530

    fix urls in service-worker and manifest

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,25 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var reload = browserSync.reload;
-var sass = require('gulp-sass');
+//var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
 var minifyCss = require('gulp-minify-css');
 var rename = require('gulp-rename');
 var del = require('del');
 var swPrecache = require('sw-precache');
 
-gulp.task('sass', function () {
-  return gulp
-    .src('./styles/*.scss')
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(gulp.dest('./styles/'))
-    .pipe(minifyCss({}))
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('./styles/'));
-});
+// gulp.task('sass', function() {
+//   return gulp
+//     .src('./styles/*.scss')
+//     .pipe(sass())
+//     .pipe(autoprefixer())
+//     .pipe(gulp.dest('./styles/'))
+//     .pipe(minifyCss({}))
+//     .pipe(rename({
+//       suffix: '.min'
+//     }))
+//     .pipe(gulp.dest('./styles/'));
+// });
 
 gulp.task('generate-sw', function() {
   var swOptions = {
@@ -42,7 +44,7 @@ gulp.task('generate-sw', function() {
 });
 
 gulp.task('serve', ['generate-sw'], function() {
-  gulp.watch('./styles/*.scss', ['sass']);
+  //gulp.watch('./styles/*.scss', ['sass']);
   browserSync({
     notify: false,
     logPrefix: 'weatherPWA',
@@ -52,7 +54,7 @@ gulp.task('serve', ['generate-sw'], function() {
   gulp.watch([
     './*.html',
     './scripts/*.js',
-    './styles/*.css',
+    //'./styles/*.css',
     '!./service-worker.js',
     '!./gulpfile.js'
   ], ['generate-sw'], browserSync.reload);
